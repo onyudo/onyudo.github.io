3592a2c720a5abf42c43425e1015e0e16ac2f025
“Martin <martin@forceandform.com>
2025-04-08 13:22:39 -0400

    Updated sorter.js file with commented out pseudocode for basic JavaScript structure.

--- js/sorter.js
+++ js/sorter.js
@@ -1,1 +1,9 @@
 "use strict";
+
+// create const variable for buttons? or maybe a form selector?
+
+// some kind of querySelector for all portoflio items?
+
+// buttons and eventListeners
+
+    //forEach to display or not disply portfolio itemms based on category
