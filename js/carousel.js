c903a053a40bb4737432664f484ab0db09d678df
“Martin <martin@forceandform.com>
2025-04-08 13:15:54 -0400

    Upidated carousel.js file with commented out pseudocode for basic JavaScript structure.

--- js/carousel.js
+++ js/carousel.js
@@ -1,1 +1,13 @@
 "use strict";
+
+// set each slide/image to document.querySelectorAll to HMTL class slide
+
+// create a function
+
+    // loop through the slides with a forEach?
+
+    // increment slides somehow
+
+    // use setTimeout to set how long each slide should be displayed
+
+// call function defined above
