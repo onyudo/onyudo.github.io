3cc06b4bb465f1eb78c3e552199cc57261dd6dcd
“Martin <martin@forceandform.com>
2025-05-20 14:12:02 -0400

    Created portfolio sorting JavaScript for homepage. Added placeholder content for testing purposes only while real content is still being generated.

--- js/sorter.js
+++ js/sorter.js
@@ -1,9 +1,23 @@
 "use strict";
 
 // create const variable for buttons? or maybe a form selector?
+const buttons = document.querySelectorAll('.filter-button');
+const items = document.querySelectorAll('.portfolio-item');
 
 // some kind of querySelector for all portoflio items?
 
 // buttons and eventListeners
+buttons.forEach(button => {
+  button.addEventListener('click', function() {
+    const category = this.getAttribute('data-category');
 
     //forEach to display or not disply portfolio itemms based on category
+    items.forEach(item => {
+      if (category === 'all' || item.classList.contains(category)) {
+        item.style.display = 'block';
+      } else {
+        item.style.display = 'none';
+      }
+    });
+  });
+});
